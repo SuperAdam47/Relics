def5cc46402b7450ad8d6b794e7cc39e47f66f79
DuooIncc <37597972+DuooIncc@users.noreply.github.com>
2018-06-01 14:01:23 +0100

    Update Main.php

--- src/Relics/Main.php
+++ src/Relics/Main.php
@@ -15,71 +15,86 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener{
-	
-	
-		public function onEnable() {
-			
-			$this->getServer()->getPluginManager()->registerEvents($this,$this);
-			$this->saveDefaultConfig();
-			$this->getServer()->getLogger()->notice(TF::AQUA . TF::BOLD . "Relics" . TF::RESET . TF::GRAY . "has been loaded! Plugin by DuooIncc");
-			$this->getServer()->getLogger()->notice(TF::GRAY . "Github: https://github.com/DuooIncc");
-			$this->getServer()->getLogger()->notice(TF::GRAY . "Twitter: https://twitter.com/duooincurr");
-		
-	}
-	
-		public function onDisable() {
-		
-			$this->getServer()->getLogger()->warning(TF::AQUA . TF::BOLD . "Relics" . TF::RESET . TF::GRAY . "has been unloaded!");
-		
-	}
-	
-		public function onBreak(BlockBreakEvent $event) {
-	//////////////////////// Relic ////////////////////////
-		
-			if($event->getBlock()->getId() == 1) {
-			
-				if(mt_rand(1, 50) === 10) {
-		
-					$player = $event->getPlayer();
-					$name = $player->getName();
-			
-					$tier1 = Item::get(Item::NETHER_STAR, 0, 1);
-					$tier1->setCustomName(TF::RESET . TF::GOLD . TF::BOLD . "Relic" . TF::RESET . TF::GRAY . " (Click)" . PHP_EOL .
-					TF::GRAY . " * " . TF::GREEN . "A treasure found by mining stone" . PHP_EOL .
-					TF::GRAY . " * " . TF::GREEN . "Tap anywhere to see what it holds");
-		
-					$player->getInventory()->addItem($tier1);
-					$player->addtitle(TF::GRAY . "You have found a", TF::GOLD . TF::BOLD . "Relic" . TF::RESET);
-					$this->getServer()->broadcastMessage(TF::RED . TF::BOLD . "(!) " . TF::RESET . TF::GREEN . $name . TF::GRAY . " has found a " . TF::GOLD . TF::BOLD . "Relic");
-					
-				}
-			
-			}
-		}
-	//////////////////////// Open Relic ////////////////////////
-	
-		public function onTap(PlayerInteractEvent $event) {
-	
-			$player = $event->getPlayer();
-			$item = $player->getInventory()->getItemInHand();
-  
-				if($item->getName() == TF::RESET . TF::GOLD . TF::BOLD . "Relic" . TF::RESET . TF::GRAY . " (Click)" . PHP_EOL . TF::GRAY . " * " . TF::GREEN . "A treasure found by mining stone" . PHP_EOL . TF::GRAY . " * " . TF::GREEN . "Tap anywhere to see what it holds") {
-		
-					$player->getInventory()->removeItem(Item::get(Item::NETHER_STAR, 0, 1));
-					$player->addTitle(TF::GRAY . "Uncovering", TF::GOLD . TF::BOLD . "Relic");
-					
-					$open = $player->addTitle(TF::GREEN . "You have received", TF::GREEN . "your rewards!");
-					
-					$this->getServer()->getScheduler()->scheduleDelayedTask($open, 100);
-					
-					
-				foreach($this->getConfig()->get("relic-loot") as $rewards){
-					
-					$give = $player->getInventory()->addItem(Item::get($rewards, 0, mt_rand(1,$this->getConfig()->get("relic-max"))));
-					
-					$this->getServer()->getScheduler()->scheduleDelayedTask($give, 500);
-			}
-		}
-	}
-}
+class Main extends PluginBase implements Listener
+{
+
+
+    public function onEnable()
+    {
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->getServer()->getLogger()->notice(TF::AQUA . TF::BOLD . "Relics" . TF::RESET . TF::GRAY . " has been loaded! Plugin by DuooIncc");
+        $this->getServer()->getLogger()->notice(TF::GRAY . "Github: https://github.com/DuooIncc");
+        $this->getServer()->getLogger()->notice(TF::GRAY . "Twitter: https://twitter.com/duooincurr");
+
+    }
+
+    public function onDisable()
+    {
+
+        $this->getServer()->getLogger()->warning(TF::AQUA . TF::BOLD . "Relics" . TF::RESET . TF::GRAY . "has been unloaded!");
+
+    }
+
+    public function onBreak(BlockBreakEvent $event)
+    {
+        //////////////////////// Relic ////////////////////////
+
+        if ($event->getBlock()->getId() == 1) {
+
+            if (mt_rand(1, 50) === 10) {
+
+                $player = $event->getPlayer();
+                $name = $player->getName();
+
+                $tier1 = Item::get(Item::NETHER_STAR, 0, 1);
+                $tier1->setCustomName(TF::RESET . TF::GOLD . TF::BOLD . "Common Relic" . TF::RESET . TF::GRAY . " (Click)" . PHP_EOL .
+                    TF::GRAY . " * " . TF::GREEN . "A treasure found by mining" . PHP_EOL .
+                    TF::GRAY . " * " . TF::GREEN . "Tap anywhere to see what it holds");
+
+                $player->getInventory()->addItem($tier1);
+                $player->addtitle(TF::GOLD . "Relic Found!!", TF::GREEN . TF::BOLD . "Wonder what's inside?" . TF::RESET);
+                $this->getServer()->broadcastMessage(TF::RED . TF::BOLD . "(!) " . TF::RESET . TF::GREEN . $name . TF::GRAY . " has found a " . TF::GOLD . TF::BOLD . "Common Relic");
+
+                if (mt_rand(1, 500) === 10) {
+
+                    $player = $event->getPlayer();
+                    $name = $player->getName();
+
+                    $tier2 = Item::get(Item::NETHER_STAR, 0, 1);
+                    $tier2->setCustomName(TF::RESET . TF::GOLD . TF::BOLD . "Uncommon Relic" . TF::RESET . TF::GRAY . " (Click)" .
+                        TF::GRAY . " * " . TF::GREEN . "A treasure found by mining" .
+                        TF::GRAY . " * " . TF::GREEN . "Tap anywhere to see what it holds");
+
+                    $player->getInventory()->addItem($tier2);
+                    $player->addtitle(TF::GOLD . "Relic Found!!", TF::GREEN . TF::BOLD . "Wonder what's inside?" . TF::RESET);
+                    $this->getServer()->broadcastMessage(TF::RED . TF::BOLD . "(!) " . TF::RESET . TF::GREEN . $name . TF::GRAY . " has found an " . TF::GOLD . TF::BOLD . "Uncommon Relic");
+
+                }
+            }
+        }
+    }
+
+    //////////////////////// Open Relic ////////////////////////
+
+    public function onTap(PlayerInteractEvent $event){
+
+        $player = $event->getPlayer();
+        $item = $player->getInventory()->getItemInHand();
+        $give = $player->getInventory()->addItem();
+
+        if ($item->getName() == TF::RESET . TF::GOLD . TF::BOLD . "Common Relic" . TF::RESET . TF::GRAY . " (Click)" . PHP_EOL .
+            TF::GRAY . " * " . TF::GREEN . "A treasure found by mining" . PHP_EOL .
+            TF::GRAY . " * " . TF::GREEN . "Tap anywhere to see what it holds") {
+
+                foreach ($this->getConfig()->get("relic-loot") as $rewards) {
+
+                    if($item instanceof PlayerInteractEvent)
+                    $give = $player->getInventory()->addItem(Item::get($rewards(0,mt_rand($this->getConfig()->get("relic-max")))));
+                    $this->getServer()->getScheduler()->scheduleDelayedTask($give, 100);
+                    }
+                }
+            }
+        }
+
